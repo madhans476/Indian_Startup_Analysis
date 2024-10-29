7a2b54ae4761ca87751e2e64cd009d40bf2197d2
Madhan S <madhanmuthu1708@gmail.com>
2024-10-29 19:51:26 +0530

    Updated VERTICAL.py

--- Pages/VERTICAL.py
+++ Pages/VERTICAL.py
@@ -1,155 +1,156 @@
 import streamlit as st
 import pandas as pd
-import seaborn as sns 
-import matplotlib.pyplot as plt
-import altair as alt
+from functions import DataFrame, Barplot, bar_line_and_df
 
-st.set_page_config(layout='wide',page_title="Major Startup Verticals In India")
-
-tab1,tab2 = st.tabs(["Major Verticals","Compare Verticals"])
-st.write()
+st.set_page_config(layout='wide', page_title="Major Startup Verticals In India")
 
 df = pd.read_csv("startup_funding.csv")
 
-
-def line_plot(df,x,y):
-    fig, ax = plt.subplots(figsize=(6, 2.5))
-    fig.patch.set_facecolor('#0E1117') 
-    ax.set_facecolor('#262730')
-    sns.lineplot(data=df, x=x, y=y,color="Orange", ax=ax)
-
-    ax.xaxis.set_tick_params(color='white')  
-    ax.yaxis.set_tick_params(color='white')  
-    ax.tick_params(axis='x', colors='white', labelsize=4)  
-    ax.tick_params(axis='y', colors='white', labelsize=4)
-    ax.xaxis.label.set_color('white')        
-    ax.yaxis.label.set_color('white')        
-    ax.title.set_color('white')
-    st.pyplot(fig)
-    
-def bar_plot(df,x,y):
-    fig, ax = plt.subplots(figsize=(6, 2.5))
-    fig.patch.set_facecolor('#0E1117') 
-    ax.set_facecolor('#262730')
-    axis = sns.barplot(data=df, x=x, y=y, color="Orange", ax=ax)
-
-    for container in axis.containers:
-        axis.bar_label(container, fmt='%d', padding=3, color='white',fontsize = 5)
-    ax.xaxis.set_tick_params(color='white',)  
-    ax.yaxis.set_tick_params(color='white')  
-    ax.tick_params(axis='x', colors='white',labelsize=5)  
-    ax.tick_params(axis='y', colors='white',labelsize=5)
-    ax.xaxis.label.set_color('white')        
-    ax.yaxis.label.set_color('white')        
-    ax.title.set_color('white')
-    # ax.set_xscale('log', base=2)
-    st.pyplot(fig)
-    
-## TAB 1 Functions
-# @st.cache_data
-def funding_bar_plot(n):
-    gk=df.groupby('vertical')['amount'].sum().sort_values(ascending=False)
-    gk=gk.reset_index()
+def funding_bar_plot(n, choice):
+    gk = df.groupby('vertical')['amount'].sum().sort_values(ascending=False).reset_index()
     gk = gk.iloc[0:n]
-    bar_plot(gk,'amount','vertical')
-    
-    
+    if choice == "Barplot":
+        Barplot(n, gk, 'amount', 'vertical', 1)
+    else:
+        DataFrame(n, gk, amount=True)
+
+def funding_bar_plot_2(n, choice):
+    gk = df.groupby('vertical')['amount'].sum().sort_values(ascending=True).reset_index()
+    gk = gk.iloc[0:n]
+    if choice == "Barplot":
+        Barplot(n, gk, 'amount', 'vertical', 1, False, True)
+    else:
+        DataFrame(n, gk, amount=True)
+
 # @st.cache_data    
-def year_wise(year):
-    gk=df.groupby(['vertical','year'])['startup'].count().sort_values(ascending=False)
-    gk=gk.reset_index()
-    gk = gk[gk['year']==year]
-    sorted_df = gk.sort_values(by='startup', ascending=False)
-    sorted_df = sorted_df.iloc[0:10]
-    bar_plot(sorted_df,"startup","vertical")
+def year_wise(year, check,n):
+    gk = df.groupby(['vertical', 'year'])['startup'].count().reset_index(name='count')
+    gk = gk[gk['year'] == year].sort_values(by='count', ascending=check).iloc[0:n]
+    gk = gk.reset_index(drop=True)
+    gk.index = gk.index + 1
+    default_option = 'BarPlot' if gk['count'][n] > gk['count'][1] else 'DataFrame'
+    if check == True:
+        choice = st.selectbox("Type: ", ("Barplot", "DataFrame"), key="id_2" ,index=['BarPlot','DataFrame'].index(default_option))
+    else:
+        choice = st.selectbox("Type: ", ("Barplot", "DataFrame"), key="id_5")
+    if choice == "Barplot":
+        Barplot(n, gk, 'count', 'vertical', 1, False, check)
+    else:
+        DataFrame(n, gk)
 
 # @st.cache_data      
-def vertical_analysis(choice,selection):
-    df_filtered = df[df['vertical']==choice]
-    df_filtered = df_filtered.groupby('year')['startup'].count()
-    df_filtered = df_filtered.reset_index()
-    df_filtered['year'] = df_filtered['year'].astype(str)
-    if selection == "DataFrame":
-        return df_filtered
-    line_plot(df_filtered,'year','startup')
-    
-    
-def funding_analysis(choice,selection):
-    gk = df.groupby(['vertical','year'])['amount'].sum()
-    gk = gk.reset_index()
-    gk = gk[gk['vertical']==choice]
-    gk['year']=gk['year'].astype(str)
-    if selection=="DataFrame":
-        return gk
-    line_plot(gk,'year','amount')
+def vertical_analysis(choice, selection):
+    df_filtered = df[df['vertical'] == choice]
+    data2 = df_filtered['year'].value_counts()
+    bar_line_and_df(data2, selection, "count")
+
+def funding_analysis(choice, selection):
+    gk = df[df['vertical'] == choice].groupby(['year'])['amount'].sum()
+    bar_line_and_df(gk, selection, 'Amount Funded')
 
 ## Tab 1
-with tab1:
-    ## Barplot based on sum funding
-    st.header("Top Verticals In India")
-    st.subheader("Most Funded Verticals")
-    n = st.slider("Select n:",2,25,5)
-    funding_bar_plot(n)
-    st.write("\n")
-    
-    ## Barplot based on no of startups in every vertical per year
-    st.subheader("Top verticals during a given year")
-    choice=st.selectbox("Select a year:",[x for x in range(2015,2021)])
-    st.write("\n")
-    year_wise(choice)
-    
-    
-    st.write("\n")
-    st.header("Vertical Analysis")
-    gk=df.groupby('vertical')['amount'].sum().sort_values(ascending=False)
-    gk=gk.reset_index()
-    choice = st.selectbox("Choose a vertical:",gk['vertical'].iloc[0:25])
-    
-    st.subheader(f"Major Hubs Of {choice}")
-    df['city'] = df['city'].str.replace(" ","",regex=False)
-    
-    fk = df[df['vertical']==choice]
-    citi = pd.read_csv('cities.csv')
-    citi['city'] = citi['city'].str.replace(" ","",regex=False)
-    # citi.to_csv('cities.csv')
-    merged_data = citi[citi['city'].isin(fk['city'])]
-    
-    if 'lat' in merged_data.columns and 'lon' in merged_data.columns:
-        st.map(merged_data[['lat', 'lon']], color='#86FD02')
-    else:
-        st.error("The city data is missing 'latitude' and 'longitude' columns.")
-    
-    
-    col1,col2 = st.columns(2)
-    with col1:
-        ## Vertical Wise Analysis
-        st.subheader("Number of Startups vs Year")
-        selection = st.selectbox("Options",['Lineplot','DataFrame'],key='selectbox2')
-        if(selection=="Lineplot"):
-            vertical_analysis(choice,selection)
-            
-        else:
-            
-            data_frame = vertical_analysis(choice,selection)
-            st.dataframe(data_frame.style.set_properties(**{'background-color': '#262730','text-align': 'center',}))
-        st.write("\n")
-    
-    
-    with col2:
-        ## Funding analysis
-        st.subheader("Funding vs Year")
-        selection = st.selectbox("Options",['Lineplot','DataFrame'],key='selectbox1')
-        if(selection=="Lineplot"):
-            funding_analysis(choice,selection)
-            
-        else: 
-            data_frame = funding_analysis(choice,selection)
-            data_frame.drop(columns='vertical',inplace=True)
-            data_frame=data_frame.reset_index()
-            data_frame.drop(columns='index',inplace=True)
-            st.dataframe(data_frame.style.set_properties(**{'background-color': '#262730','text-align': 'center'}))
-        st.write("\n")
-        
-    
-    
 
+# Barplot based on sum funding
+st.title("Analysis Based On Verticals")
+st.markdown("""---""")
+st.header("Amount Based Analysis")
+st.subheader("Most Funded Verticals")
+choice1 = st.selectbox("Type: ", ("Barplot", "DataFrame"))
+df.columns = df.columns.str.replace(" ", "", regex=False)
+n = st.slider("Select n:", 2, 45, 5)
+funding_bar_plot(n, choice1)
+st.write("\n")
+
+# Least funded
+st.subheader("Least Funded Verticals")
+choice1 = st.selectbox("", ("Barplot", "DataFrame"), key='id_3')
+df.columns = df.columns.str.replace(" ", "", regex=False)
+n = st.slider("Select n:", 2, 45, 5, key="id_4")
+funding_bar_plot_2(n, choice1)
+st.write("\n")
+
+st.markdown("""---""")
+
+# Barplot based on number of startups in every vertical per year
+st.header("Year Wise Analysis")
+choice = st.selectbox("Select a year:", [x for x in range(2015, 2022)])
+st.subheader(f"Top Verticals In {choice}")
+n = st.slider("Select n:", 2, 20, 5, key="id_8")
+st.write("\n")
+year_wise(choice, False,n)
+
+# Least Verticals in a year
+
+# choice = st.selectbox("Select a year:", [x for x in range(2015, 2021)], key="id_6")
+st.subheader(f"Least Verticals In {choice}")
+# n = st.slider("Select n:", 2, 20, 5, key="id_9")
+# choice2 = st.selectbox("Type: ", ("Barplot", "DataFrame"), key="id_5")
+st.write("\n")
+year_wise(choice, True,n)
+
+st.markdown("""---""")
+st.write("\n")
+st.header("Vertical Analysis")
+gk = df.groupby('vertical')['amount'].sum().sort_values(ascending=False).reset_index()
+choice = st.selectbox("Select a vertical:", gk['vertical'])
+data = df[df['vertical'] == choice]
+ct =data['startup'].count()
+st.subheader(f"# Startups from :orange-background[{choice}] : {ct}")
+
+st.subheader('Year Wise Analysis:')
+col1, col2 = st.columns(2)
+with col1:
+    # Vertical Wise Analysis
+    st.subheader("Number Of Fundings Year Wise")
+    selection = st.selectbox("Options", ['Lineplot', 'DataFrame', 'Barplot'], key='selectbox2')
+    vertical_analysis(choice, selection)
+    st.write("\n")
+
+with col2:
+    # Funding analysis
+    st.subheader("Amount Funded Year Wise")
+    selection = st.selectbox("Options", ['Lineplot', 'DataFrame', 'Barplot'], key='selectbox1')
+    funding_analysis(choice, selection)
+
+#! -------------------------------------------------------------------------------------------------------------------
+
+st.subheader(f"Major Hubs Of {choice}")
+df['city'] = df['city'].str.replace(" ", "", regex=False)
+
+fk = df[df['vertical'] == choice]
+citi = pd.read_csv('cities.csv')
+citi['city'] = citi['city'].str.replace(" ", "", regex=False)
+merged_data = citi[citi['city'].isin(fk['city'])]
+
+if 'lat' in merged_data.columns and 'lon' in merged_data.columns:
+    st.map(merged_data[['lat', 'lon']], color='#86FD02')
+else:
+    st.error("The city data is missing 'latitude' and 'longitude' columns.")
+
+
+st.markdown("""---""")
+st.header('Compare Verticaals')
+vertical_names = sorted(list(df['vertical'].unique()))
+selected_vertical = st.multiselect('Select the states:', vertical_names, default=[vertical_names[i] for i in [8, 11]])
+from functions import grouped_bar, lollipop, grouped_df
+
+st.subheader("Amount Funded")
+type1 = st.selectbox('Type:',['Lollipop Plot', 'BarPlot','DataFrame'], key='ver61')
+if type1 == 'BarPlot':
+    grouped_bar(selected_vertical,df,'vertical','amount')
+elif type1 == "Lollipop Plot":
+    lollipop(selected_vertical, df, 'vertical', 'amount')
+else: 
+    grouped_df(selected_vertical, df, 'vertical', 'amount')
+
+
+st.subheader("# Fundings")
+type2 = st.selectbox('Type:',['Lollipop Plot', 'BarPlot', 'DataFrame'], key='ver62')
+if type2 == 'BarPlot':
+    grouped_bar(selected_vertical,df,'vertical','count')
+elif type2 == "Lollipop Plot":
+    lollipop(selected_vertical, df, 'vertical', 'count')
+else: 
+    grouped_df(selected_vertical, df, 'vertical', 'count')
+
+st.markdown("""---""")
